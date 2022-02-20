352acbe1abbc3b6ee8f1539f71e6713538409b2a
acul021 <tucci.bimmelt@gmail.com>
2022-02-20 16:05:34 +0000

    Auto-Commit - Changed in other repo, updating testfiles

---
+++ src/test/java/edu/kit/informatik/DependencyTestByAcul21.java
@@ -0,0 +1,42 @@
+package edu.kit.informatik;
+
+import edu.kit.informatik.exception.ParseException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class DependencyTest {
+
+    @Test
+    public void testSomething() {
+
+    }
+
+
+    private Network net(String net) {
+        try {
+            return new Network(net);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private List<IP> ips(String... ips) {
+        return this.ip(ips);
+    }
+
+    private List<IP> ip(String... ips) {
+        return Arrays.stream(ips).map(this::ip).collect(Collectors.toList());
+    }
+
+    private IP ip(String ip) {
+        try {
+            return new IP(ip);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
