f78e4fb04fa88f9ebe12565cdb41f989a83b342a
acul021 <tucci.bimmelt@gmail.com>
2022-02-24 21:26:53 +0100

    Renamed file
    Es wäre hier echt nett gewesen, wenn du mich gefragt hättest/im file erwähnt hättest, dass es von mir kommt.

--- src/test/java/edu/kit/informatik/Acul021TestUtils.java
+++ src/test/java/edu/kit/informatik/Acul021TestUtils.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class KoeriTestUtils {
+public class Acul021TestUtils {
     public static final String SMALL_NET
         = "(85.193.148.81 (141.255.1.133 122.117.67.158 0.146.197.108) 34.49.145.239 "
         + "(231.189.0.127 77.135.84.171 39.20.222.120 252.29.23.0 116.132.83.77))";
